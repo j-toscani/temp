6b25140d0a0836563ace6be6ce270da7de15013f
Julian Toscani <julian_toscani@gmx.de>
2021-12-11 22:22:17 +0100

    extract struct from main function

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,37 @@
-use std::fs;
-use std::io;
-use std::path::{Path, PathBuf};
+use temp::Config;
 
 fn main() {
     let action = "create";
     let outpath = "components";
     let filename = "hello.txt";
-    let content = "Hello World!";
 
     let args = vec![action, outpath, filename];
-    let out = PathBuf::from_iter([args[1], args[2]]);
 
-    let parent = match out.parent() {
-        Some(val) => val,
-        None => Path::new("/"),
-    };
+    Config::new($args);
+    // let out = PathBuf::from_iter([args[1], args[2]]);
 
-    if !parent.exists() {
-        let mut input = String::new();
-        println!("Directory does not exist. Do you want it to be created? [Y/N]");
-        let should_create = match io::stdin().read_line(&mut input) {
-            Ok(_n) => input.trim().to_lowercase() == String::from("y"),
-            Err(_) => false,
-        };
-        if should_create {
-            match fs::create_dir_all(parent) {
-                Ok(()) => (),
-                Err(_) => panic!("Directory could not be created"),
-            };
-        } else {
-            println!("Directory was not created.");
-            return;
-        }
-    }
+    // let parent = match out.parent() {
+    //     Some(val) => val,
+    //     None => Path::new("/"),
+    // };
 
-    fs::write(out.as_path(), content).expect("Could not write File.");
+    // if !parent.exists() {
+    //     let mut input = String::new();
+    //     println!("Directory does not exist. Do you want it to be created? [Y/N]");
+    //     let should_create = match io::stdin().read_line(&mut input) {
+    //         Ok(_n) => input.trim().to_lowercase() == String::from("y"),
+    //         Err(_) => false,
+    //     };
+    //     if should_create {
+    //         match fs::create_dir_all(parent) {
+    //             Ok(()) => (),
+    //             Err(_) => panic!("Directory could not be created"),
+    //         };
+    //     } else {
+    //         println!("Directory was not created.");
+    //         return;
+    //     }
+    // }
+
+    // fs::write(out.as_path(), content).expect("Could not write File.");
 }
