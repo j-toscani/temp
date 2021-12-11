0fb8978dd725901911abda65b299e97007357806
Julian Toscani <julian_toscani@gmx.de>
2021-12-11 18:03:11 +0100

    check if directory exists

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,24 @@ use std::path;
 
 fn main() {
     let action = "create";
-    let outpath = "";
+    let outpath = "components";
     let filename = "hello.txt";
     let content = "Hello World!";
 
     let args = vec![action, outpath, filename];
     let out = path::PathBuf::from_iter([args[1], args[2]]);
-    fs::write(out.as_path(), content).expect("Could not write File");
+
+    let parent = match out.parent() {
+        Some(val) => val,
+        None => path::Path::new("/"),
+    };
+
+    if !parent.exists() {
+        match fs::create_dir_all(parent) {
+            Ok(()) => (),
+            Err(_) => panic!("Directory could not be created"),
+        };
+    }
+
+    fs::write(out.as_path(), content).expect("Could not write File.");
 }
