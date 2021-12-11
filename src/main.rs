021fa8e2096112aa4142fb9ac848e33825f1f10e
Julian Toscani <julian_toscani@gmx.de>
2021-12-11 18:28:02 +0100

    ask user if directory should be created or not

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
-use std::path;
+use std::io;
+use std::path::{Path, PathBuf};
 
 fn main() {
     let action = "create";
@@ -8,18 +9,29 @@ fn main() {
     let content = "Hello World!";
 
     let args = vec![action, outpath, filename];
-    let out = path::PathBuf::from_iter([args[1], args[2]]);
+    let out = PathBuf::from_iter([args[1], args[2]]);
 
     let parent = match out.parent() {
         Some(val) => val,
-        None => path::Path::new("/"),
+        None => Path::new("/"),
     };
 
     if !parent.exists() {
-        match fs::create_dir_all(parent) {
-            Ok(()) => (),
-            Err(_) => panic!("Directory could not be created"),
+        let mut input = String::new();
+        println!("Directory does not exist. Do you want it to be created? [Y/N]");
+        let should_create = match io::stdin().read_line(&mut input) {
+            Ok(_n) => input.trim().to_lowercase() == String::from("y"),
+            Err(_) => false,
         };
+        if should_create {
+            match fs::create_dir_all(parent) {
+                Ok(()) => (),
+                Err(_) => panic!("Directory could not be created"),
+            };
+        } else {
+            println!("Directory was not created.");
+            return;
+        }
     }
 
     fs::write(out.as_path(), content).expect("Could not write File.");
