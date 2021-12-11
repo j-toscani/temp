1e08152363d1adf49d6b580e0750eec011e5304e
Julian Toscani <toscani@cekom.de>
2021-12-12 00:34:15 +0100

    add run function and use env arguments to run config

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,65 @@
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
+use std::error::Error;
+use std::fs::{create_dir_all, write};
 
+#[derive(Debug)]
 enum Action {
     CREATE,
     ADD,
 }
 
+#[derive(Debug)]
 pub struct Config {
     action: Action,
-    template_key: String,
+    // template_key: String,
     outpath: PathBuf,
-    filename: String,
 }
 
 impl Config {
-    pub fn new(args: Vec<&str>) -> Result<Config, &str> {
-        if args.len() < 2 {
-            return Err("Please provide an action and a template_key.");
+    pub fn new(args: &Vec<String>) -> Result<Config, &str> {
+        if args.len() < 4 {
+            return Err("Please provide an action, a template_key and an outputpath.");
         }
 
-        let action = match args[0] {
+        let action = match args[1].as_str().trim() {
             "create" => Action::CREATE,
             "add" => Action::ADD,
             _ => return Err("This this action does not exist"),
         };
 
-        let template_key = String::from(args[1]);
+        // let template_key = String::from(args[2]);
 
-        let outpath = match args.len() >= 3 {
-            true => PathBuf::from(args[2]),
-            false => return Err("No Output directory specified."),
+        let mut outpath = PathBuf::from(args[3].as_str());
+
+        let filename = match args.len() >= 5 {
+            true => args[4].as_str(),
+            false => "New.txt",
         };
 
-        let filename = match args.len() >= 4 {
-            true => args[3],
-            false => "New",
-        };
+        outpath.push(filename);
 
         Ok(Config {
             action,
-            template_key,
+            // template_key,
             outpath,
-            filename: String::from(filename),
         })
     }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let parent = match config.outpath.parent() {
+        Some(path) => path,
+        None => Path::new("/")
+    };
+
+    if !parent.exists() {
+        create_dir_all(parent)?;
+    }
+
+    match config.action {
+        Action::ADD => println!("Using Create..."),
+        Action::CREATE => write(config.outpath, "Hello!")?
+    }
+
+    Ok(())
 }
