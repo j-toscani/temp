990589cf0ac4776621c355ea2cc3a4bae9ceb2d5
Julian Toscani <julian_toscani@gmx.de>
2021-12-11 17:36:26 +0100

    setup example usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs;
+use std::path;
+
 fn main() {
-    println!("Hello, world!");
+    let action = "create";
+    let outpath = "/";
+    let filename = "hello.txt";
+    let content = "Hello World!";
+
+    let args = vec![action, outpath, filename];
+    let out = path::PathBuf::from_iter([args[1], args[2]]);
+    fs::write(out.as_path(), content).expect("Could not write File");
 }
