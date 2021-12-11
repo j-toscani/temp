85e73e6243acf121d1fbe9933cb9845bc049ed02
Julian Toscani <toscani@cekom.de>
2021-12-12 00:33:28 +0100

    add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,17 @@
-use temp::Config;
+use temp::{Config, run};
+use std::process;
+use std::env::args;
 
 fn main() {
-    let action = "create";
-    let outpath = "components";
-    let filename = "hello.txt";
+    let args: Vec<String> = args().collect();
 
-    let args = vec![action, outpath, filename];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    Config::new($args);
-    // let out = PathBuf::from_iter([args[1], args[2]]);
-
-    // let parent = match out.parent() {
-    //     Some(val) => val,
-    //     None => Path::new("/"),
-    // };
-
-    // if !parent.exists() {
-    //     let mut input = String::new();
-    //     println!("Directory does not exist. Do you want it to be created? [Y/N]");
-    //     let should_create = match io::stdin().read_line(&mut input) {
-    //         Ok(_n) => input.trim().to_lowercase() == String::from("y"),
-    //         Err(_) => false,
-    //     };
-    //     if should_create {
-    //         match fs::create_dir_all(parent) {
-    //             Ok(()) => (),
-    //             Err(_) => panic!("Directory could not be created"),
-    //         };
-    //     } else {
-    //         println!("Directory was not created.");
-    //         return;
-    //     }
-    // }
-
-    // fs::write(out.as_path(), content).expect("Could not write File.");
+    run(config).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
 }
